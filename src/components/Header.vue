25a1aef1b2205d6368da479e68f4ada70a23d72b
Martin Iliev <xxsuperdreamxx@gmail.com>
2023-06-19 12:34:20 +0200

    fixed icon and few conflicts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -93,9 +93,7 @@
           <a href="#"
              class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto"
-                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                 alt="" />
+            <Logo class="h-8 w-auto" />
           </a>
           <button type="button"
                   class="-m-2.5 rounded-md p-2.5 text-gray-700"
